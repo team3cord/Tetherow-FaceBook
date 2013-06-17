7f647c0bea5ce2587d41aaf8ffdae45fb559c87e
mattc <mattc@summitprojects.com>
2013-06-17 16:49:43 -0700

    Added first layer tabs on LIVE pg

--- fb/live.php
+++ fb/live.php
@@ -14,10 +14,27 @@
 
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <link href="js/css/first/jquery-ui-1.10.3.custom.css" rel="stylesheet">
+        <script src="js/vendor/jquery-1.9.1.min.js"></script>
+        <script src="js/vendor/jquery-ui-1.10.3.custom.js"></script>  
+        <script>
+            $(function() {
+                
+                $( "#tabs" ).tabs();
 
-         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+                // Hover states on the static widgets
+                $( "#dialog-link, #icons li" ).hover(
+                    function() {
+                        $( this ).addClass( "ui-state-hover" );
+                    },
+                    function() {
+                        $( this ).removeClass( "ui-state-hover" );
+                    }
+                );
+            });
+        </script>
+         
+        
         
 
 
@@ -38,6 +55,21 @@
         	<a id="nv-e" href="/events.php"><span>EVENTS</span></a>
         	<a id="nv-s" href="/stay.php"><span>STAY</span></a>
         </nav>
+        <div id="tabs" class="tabbed-content">
+            <ul class="top-tab">
+                <li><a href="#tabs-1">Tab One</a></li>
+                <li><a href="#tabs-2">Tab Two</a></li>
+                <li><a href="#tabs-3">Tab Three</a></li>
+            </ul>
+            <div id="tabs-1"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></div>
+            <div id="tabs-2"><p>Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.</p></div>
+            <div id="tabs-3"><p>Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.</p></div>
+        </div>
+
+
+        <div class="real-link">
+            <a href="#">View Real Estate</a>
+        </div>
       </div> 
 
 
@@ -46,13 +78,8 @@
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
-        // $(function () {
-        //     $('#nv-l').
-        // });
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
+            
+            
         </script>
     </body>
 </html>
