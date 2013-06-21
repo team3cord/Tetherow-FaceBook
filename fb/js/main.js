d2899513992c7c837213339d50f8cf28f8b9fde8
MattCordeiro <matthew3cord@gmail.com>
2013-06-21 15:31:03 -0700

    changed format to html

---
+++ fb/js/main.js
@@ -0,0 +1,21 @@
+
+$(function() {
+
+    $( "#tabs" ).tabs();
+
+    // Hover states on the static widgets
+    $( "#dialog-link, #icons li" ).hover(
+        function() {
+            $( this ).addClass( "ui-state-hover" );
+        },
+        function() {
+            $( this ).removeClass( "ui-state-hover" );
+        }
+    );
+});
+
+$(function(){
+    $('#header').load('includes/header.html #hdr');
+    console.log(this);
+
+});
